b027104bebe6f82179e53533c55e4cc6d77a2714
christo jojo <christojojo40@gmail.com>
2024-03-25 12:30:24 +0530

    y axis line

--- src/pages/LinechartPage.jsx
+++ src/pages/LinechartPage.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import LineChart from '../components/LineChart/LineChart'
+import MultiLineChart from '../components/MultiLineChart/MultiLineChart'
 
 function LinechartPage() {
   return (
     <div className='pt-20'>
+      <div>
       <LineChart/>
+      </div>
+      <div className='pt-16'>
+        <h1 className='mb-12 font-medium text-lg'>MultiLine -Chart</h1>
+        <MultiLineChart></MultiLineChart>
+      </div>
     </div>
   )
 }

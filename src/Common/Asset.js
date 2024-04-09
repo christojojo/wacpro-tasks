3820509dcab6c043a67156a74da4d154809504a0
christo jojo <christojojo40@gmail.com>
2024-04-09 13:38:50 +0530

    feedbackgraph change

--- src/Common/Asset.js
+++ src/Common/Asset.js
@@ -2,13 +2,18 @@ const img1 = "/images/card-chart-1.png";
 const line_graph = "/images/line-graph.png";
 const pie = "/images/pie.png";
 const plusBtn = "/images/plus-btn.png";
-const emoji = "/images/emoji.png";
+const smiley = "/images/smiley.png";
+const angryEmoji = "/images/angry.png";
+const mediumEmoji = "/images/medium.png";
+
 
 const Assets = {
                img1 ,
                line_graph    ,
                pie    ,
                plusBtn    ,
-               emoji, 
+               smiley,
+               angryEmoji,
+               mediumEmoji
             };
 export default Assets;
--- src/components/BarFeedBack/BarFeedBAck.jsx
+++ src/components/BarFeedBack/BarFeedBAck.jsx
@@ -2,13 +2,25 @@
   import Assets from "../../Common/Asset";
 
   function BarFeedBAck() {
-    const customerRating = { id: "1", Name: "christo", Rating: "0" };
+    const customerRating = { id: "1", Name: "christo", Rating: "1" };
     const totalRating = "10";
     const feedbBackValue = customerRating.Rating;
-    console.log(feedbBackValue);
+    // console.log(feedbBackValue);
     const valuePosition = Math.min(feedbBackValue / totalRating * 100, 85);
-    console.log(valuePosition, "percentage");
+    // console.log(valuePosition, "percentage");
     const leftPosition = `${valuePosition}%`;
+
+    const getImage=()=>{
+      if(valuePosition > 66){
+        return Assets.smiley
+      }
+      else if(valuePosition > 36){
+        return Assets.mediumEmoji
+      }
+      else{
+        return Assets.angryEmoji
+      }
+    } 
 
     return (
       <div className="relative w-fit">
@@ -18,7 +30,8 @@
           <li className="bg-[green] w-[65px] rounded-r-full"></li>
         </ul>
         <div className='absolute top-1/2 -translate-y-1/2 ' style={{ left: leftPosition}}>
-          <img src={Assets.emoji} alt="" />
+          {/* <img src={Assets.emoji} alt="" /> */}
+          <img src={getImage()} alt="" width={27} height={27} />
         </div>
       </div>
     );

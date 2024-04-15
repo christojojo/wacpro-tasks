d590e85997f27c234d896b0f61250dbbc1add362
christo jojo <christojojo40@gmail.com>
2024-04-15 17:48:22 +0530

    timeline partially completed

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -1,247 +1,166 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./style.css";
 
 function Timeline() {
-  const Timeline = [
-    {
-      TotalHours: "5000",HoursConsumed: "3300",ApprovedHours: "40",NotApprovedHours: "30",
-      Departments: [
-        {
-          dept: "design",
-          progress: "12%",
-          TotalHours: "300",
-          HoursConsumed: "120",
-          ApprovedHours: "0",
-          NotApprovedHours: "0",
-        },
-        {
-          dept: "Strapi",
-          progress: "27%",
-          TotalHours: "200",
-          HoursConsumed: "200",
-          ApprovedHours: "40",
-          NotApprovedHours: "10",
-        },
-        {
-          dept: "Python",
-          progress: "34%",
-          TotalHours: "30",
-          HoursConsumed: "30",
-          ApprovedHours: "0",
-          NotApprovedHours: "20",
-        },
-        {
-          dept: "Node",
-          progress: "64%",
-          TotalHours: "700",
-          HoursConsumed: "300",
-          ApprovedHours: "0",
-          NotApprovedHours: "40",
-        },
-        {
-          dept: "Testing",
-          progress: "23%",
-          TotalHours: "200",
-          HoursConsumed: "200",
-          ApprovedHours: "0",
-          NotApprovedHours: "0",
-        },
-      ],
-    },
-  ];   
+  const Timeline = {
+    TotalHours: "5000",
+    HoursConsumed: "3300",
+    ApprovedHours: "400",
+    NotApprovedHours: "300",
+    Departments: [
+      {
+        id: "1",
+        dept: "design",
+        progress: "40%",
+        TotalHours: "2000",
+        HoursConsumed: "1200",
+        // ApprovedHours: "0",
+        // NotApprovedHours: "0",
+      },
+      {
+        id: "2",
+        dept: "Strapi",
+        progress: "6%",
+        TotalHours: "300",
+        HoursConsumed: "212",
+        ApprovedHours: "40",
+        NotApprovedHours: "10",
+      },
+      {
+        id: "3",
+        dept: "Python",
+        progress: "24%",
+        TotalHours: "1200",
+        HoursConsumed: "500",
+        // ApprovedHours: "0",
+        NotApprovedHours: "20",
+      },
+      {
+        id: "4",
+        dept: "Node",
+        progress: "15%",
+        TotalHours: "750",
+        HoursConsumed: "330",
+        // ApprovedHours: "0",
+        // NotApprovedHours: "0",
+        exceed: "40",
+      },
+      {
+        id: "5",
+        dept: "Testing",
+        progress: "15%",
+        TotalHours: "750",
+        HoursConsumed: "230",
+        // ApprovedHours: "0",
+        // NotApprovedHours: "0",
+      },
+    ],
+  };
+  // let totalhourswidth;
+  // let widthApproved;
+  // let widthNotApproved;
 
-return(
-  <div>
-    {Timeline.map((item,index)=>{
-      return(
-        <div>
-        <div key={index} className="flex gap-1 pb-3">
-     <div className="bg-[#152898] text-white text-[11px] font-medium leading-[20px] py-1 pl-[9px] w-[891px] rounded ">
-       {item.HoursConsumed} / {item.TotalHours}
-     </div>
-     <div className="bg-[#A5DFAB] text-[#096E13] text-[11px] font-medium leading-[20px] py-1 pl-[6px] w-[63px] rounded">
-       {item.ApprovedHours}
-     </div>
-     <div className="not-approved-hrs bg-[#EA9354] text-[#760000] text-[11px] font-medium leading-[20px] py-1 pl-[6px] w-[56px] rounded">
-       {item.NotApprovedHours}
-     </div>
-   </div>
-    <div key={index}>{item.Departments.map((data,ind)=>{
-          return(
-           <div>
-               <div className="pb-3">
-                <div className="w-[364px] bg-[#E6E6E6] ">
-              <div className="bg-[#B3D4FF] text-[#003376] text-[11px] font-medium leading-[20px] py-1 pl-[9px] w-[244px] rounded ">
+  const [totalhourswidth, setTotalhourswidth] = useState(null);
+  const [widthApproved, setWidthApproved] = useState(null);
+  const [widthNotApproved, setWidthNotApproved] = useState(null);
+
+  const totalhoursRef = useRef(null);
+
+  useEffect(() => {
+    // const totalhours = document.querySelector(".totalhours");
+    const totalhours = totalhoursRef.current;
+    if (totalhours) {
+      const totalhourswidth = totalhours.getBoundingClientRect().width;
+      setTotalhourswidth(totalhourswidth);
+      // console.log(totalhourswidth);
+
+      const widthApproved =
+        (Timeline.ApprovedHours / Timeline.TotalHours) * totalhourswidth;
+      setWidthApproved(widthApproved);
+      // console.log(widthApproved,'approved');
+
+      const widthNotApproved =
+        (Timeline.NotApprovedHours / Timeline.TotalHours) * totalhourswidth;
+      setWidthNotApproved(widthNotApproved);
+      // console.log(widthNotApproved ,"not approved");
+    }
+  }, []);
+
+  return (
+    <>
+      <div className="flex gap-1 items-center  pb-3">
+        <div
+          className="totalhours bg-[#152898] text-white text-[11px] font-medium leading-[20px] py-1 pl-[9px] flex-grow rounded "
+          ref={totalhoursRef}
+        >
+          {Timeline.HoursConsumed} / {Timeline.TotalHours}
+        </div>
+        {Timeline.ApprovedHours && (
+          <div
+            className="appoved-hr bg-[#A5DFAB] text-[#096E13] text-[11px] font-medium leading-[20px] py-1 pl-[6px]  rounded"
+            style={{ width: widthApproved }}
+          >
+            {Timeline.ApprovedHours}
+          </div>
+        )}
+        {Timeline.NotApprovedHours && (
+          <div
+            className="not-approved-hrs bg-[#EA9354] text-[#760000] text-[11px] font-medium leading-[20px] py-1 pl-[6px] rounded "
+            style={{ width: widthNotApproved }}
+          >
+            {Timeline.NotApprovedHours}
+          </div>
+        )}
+      </div>
+
+      {Timeline.Departments.map((data, index) => {
+        const deptHour = (totalhourswidth * parseFloat(data.progress)) / 100;
+        const deptHrConsumed =
+          deptHour * (data.HoursConsumed / data.TotalHours);
+        // const deptapprovedHr = deptHour
+
+        return (
+          <div key={index} className="flex gap-1 pb-4">
+            <div
+              className="dept-total bg-[#F6F6F6]"
+              style={{ width: deptHour }}
+            >
+              <div
+                className="dept-consumed bg-[#B3D4FF] text-[#003376] text-[11px] font-medium leading-[20px] py-1 pl-[9px] rounded"
+                style={{ width: deptHrConsumed }}
+              >
                 {data.HoursConsumed} / {data.TotalHours}
-               </div>
               </div>
-           </div>
-           </div>
-          )
-        })}</div>
-   </div>
-      
-      )
-  })}
-    
-  </div>
-)
- 
+            </div>
+            {data.ApprovedHours && (
+              <div
+                className="appoved-hr bg-[#A5DFAB] text-[#096E13] text-[11px] font-medium leading-[20px] py-1 pl-[6px]  rounded"
+                style={{ width: widthApproved }}
+              >
+                +{data.ApprovedHours}
+              </div>
+            )}
+            {data.NotApprovedHours && (
+              <div
+                className="not-approved-hrs bg-[#EA9354] text-[#760000] text-[11px] font-medium leading-[20px] py-1 pl-[6px] rounded "
+                style={{ width: widthNotApproved }}
+              >
+                +{data.NotApprovedHours}
+              </div>
+            )}
+            {data.exceed && (
+              <div
+                className="bg-[#D6D6D6] text-[#515151] text-[11px] font-medium leading-[20px] py-1 pl-[6px] rounded "
+                style={{ width: widthNotApproved }}
+              >
+                +{data.exceed}
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default Timeline;
-
-
-// import React from "react";
-// import "./style.css";
-
-// function Timeline() {
-//   const Timeline = [
-//     {
-//       TotalHours: "5000",HoursConsumed: "3300",ApprovedHours: "40",NotApprovedHours: "30",
-//       Departments: [
-//         {
-//           dept: "design",
-//           progress: "12%",
-//           TotalHours: "300",
-//           HoursConsumed: "120",
-//           ApprovedHours: "0",
-//           NotApprovedHours: "0",
-//         },
-//         {
-//           dept: "Strapi",
-//           progress: "27%",
-//           TotalHours: "200",
-//           HoursConsumed: "200",
-//           ApprovedHours: "40",
-//           NotApprovedHours: "10",
-//         },
-//         {
-//           dept: "Python",
-//           progress: "34%",
-//           TotalHours: "30",
-//           HoursConsumed: "30",
-//           ApprovedHours: "0",
-//           NotApprovedHours: "20",
-//         },
-//         {
-//           dept: "Node",
-//           progress: "64%",
-//           TotalHours: "700",
-//           HoursConsumed: "300",
-//           ApprovedHours: "0",
-//           NotApprovedHours: "40",
-//         },
-//         {
-//           dept: "Testing",
-//           progress: "23%",
-//           TotalHours: "200",
-//           HoursConsumed: "200",
-//           ApprovedHours: "0",
-//           NotApprovedHours: "0",
-//         },
-//       ],
-//     },
-//   ];
-   
-
-//   Timeline[0].Departments.forEach((department) => {
-//     console.log(`Department: ${department.dept}`);
-//     console.log(`  Progress: ${department.progress}`);
-//     console.log(`  Total Hours: ${department.TotalHours}`);
-//     console.log(`  Hours Consumed: ${department.HoursConsumed}`);
-//     console.log(`  Approved Hours: ${department.ApprovedHours}`);
-//     console.log(`  Not Approved Hours: ${department.NotApprovedHours}`);
-//     console.log("---"); // Separator for each department
-//   });
-
-//   return (
-    
-//     <div className="w-fit">
-//       { Timeline.Departments.map((item,index) => {
-
-        
-
-        //   <div>
-        //      <div key={index} className="flex gap-1 pb-3">
-        //   <div className="bg-[#152898] text-white text-[11px] font-medium leading-[20px] py-1 pl-[9px] w-[891px] rounded ">
-        //     {item.HoursConsumed} / {item.TotalHours}
-        //   </div>
-        //   <div className="bg-[#A5DFAB] text-[#096E13] text-[11px] font-medium leading-[20px] py-1 pl-[6px] w-[63px] rounded">
-        //     {item.ApprovedHours}
-        //   </div>
-        //   <div className="not-approved-hrs bg-[#EA9354] text-[#760000] text-[11px] font-medium leading-[20px] py-1 pl-[6px] w-[56px] rounded">
-        //     {item.NotApprovedHours}
-        //   </div>
-        // </div>
-
-        // <div className="pb-3">
-        //    <div className="w-[364px] bg-[#E6E6E6] ">
-        //       <div className="bg-[#B3D4FF] text-[#003376] text-[11px] font-medium leading-[20px] py-1 pl-[9px] w-[244px] rounded ">
-        //         120 / 300
-        //        </div>
-        //       </div>
-        //    </div>
-
-
-        //   </div> 
-          
-
-        
-        
-        
-//       })}
-//       <div className="flex gap-1 pb-3">
-//         <div className="bg-[#152898] text-white text-[11px] font-medium leading-[20px] py-1 pl-[9px] w-[891px] rounded ">
-//           3300 / 5000
-//         </div>
-//         <div className="bg-[#A5DFAB] text-[#096E13] text-[11px] font-medium leading-[20px] py-1 pl-[6px] w-[63px] rounded">
-//           +40
-//         </div>
-//         <div className="not-approved-hrs bg-[#EA9354] text-[#760000] text-[11px] font-medium leading-[20px] py-1 pl-[6px] w-[56px] rounded">
-//           +30
-//         </div>
-//       </div>
-
-//       <div className="pb-3">
-//         <div className="w-[364px] bg-[#E6E6E6] ">
-//           <div className="bg-[#B3D4FF] text-[#003376] text-[11px] font-medium leading-[20px] py-1 pl-[9px] w-[244px] rounded ">
-//             120 / 300
-//           </div>
-//         </div>
-//       </div>
-
-//       <div className="flex gap-1 pb-3">
-//         <div className="bg-[#B3D4FF] text-[#003376] text-[11px] font-medium leading-[20px] py-1 pl-[9px] w-[365px] rounded ">
-//           200 / 200
-//         </div>
-//         <div className="flex ">
-//           <div className="bg-[#A5DFAB] text-[#096E13] text-[11px] font-medium leading-[20px] py-1 pl-[6px] w-[63px] rounded-l">
-//             +40
-//           </div>
-//           <div className="not-approved-hrs bg-[#EA9354] text-[#760000] text-[11px] font-medium leading-[20px] py-1 pl-[6px] w-[56px] rounded-r">
-//             +10
-//           </div>
-//         </div>
-//       </div>
-
-//      <div className="flex gap-1 pb-3">
-//      <div className="w-[511px] bg-[#E6E6E6] ">
-//           <div className="bg-[#B3D4FF] text-[#003376] text-[11px] font-medium leading-[20px] py-1 pl-[9px] w-[391px] rounded ">
-//             120 / 300
-//           </div>
-//         </div>
-//         <div className="bg-[#D6D6D6] text-[#515151] text-[11px] font-medium leading-[20px] py-1 pl-[6px] w-[95px] rounded">
-//             +40
-//           </div>
-//      </div>
-
-//      <div className="bg-[#B3D4FF] text-[#003376] text-[11px] font-medium leading-[20px] py-1 pl-[9px] w-[351px] rounded ">
-//             200hr
-//           </div>
-
-//     </div>
-//   );
-// }
-
-// export default Timeline;
--- src/pages/TimelinePage.jsx
+++ src/pages/TimelinePage.jsx
@@ -3,7 +3,7 @@ import Timeline from './../components/Timeline/Timeline'
 
 function TimelinePage() {
   return (
-    <div className='pt-20 w-full'>
+    <div className='pt-20 mr-[50px] w-full'>
         <h1 className='text-2xl font-semibold pb-2'>TimeLine Graph</h1>
       <Timeline/>
     </div>
